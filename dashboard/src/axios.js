28c36d6898312e8a12516b81de46ce26fd30e249
Bunyodbek <jorayevbunyodbek1505@gmail.com>
2025-01-08 08:41:22 -0800

    add vercel.json

--- dashboard/src/axios.js
+++ dashboard/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000/",
+  // baseURL: "http://localhost:9000/",
+  baseURL: "https://mern-backend-zeta.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
